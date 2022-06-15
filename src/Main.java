fe712da26716d3003805561cc4a8dc7eaa1652ae
Lose88 <hotsting1888@gmail.com>
2022-06-15 15:04:34 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int bill = 2000; // Сумма на счете
+        int replenishment = 10000; // Сумма пополнения
+        int bonus;
+        int final_bill;
+        if (replenishment > 1000)
+        {
+            bonus = replenishment / 100;
+            final_bill = bill + replenishment + bonus;
+                     System.out.println("Количество бонусов: " + bonus);
+            System.out.println("Сумма счета включая бонусы: " + final_bill);
+        }
+        else {
+            final_bill = bill + replenishment;
+            System.out.println("Количество бонусов 0");
+            System.out.println("Сумма счета: " + final_bill);
+
+        }
+
+
     }
 }
