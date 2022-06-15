660480b80e7007fa3f8b5e1d15c8a93efb8be454
Lose88 <hotsting1888@gmail.com>
2022-06-15 21:22:46 +0300

    Format

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,16 @@ public class Main {
         int bill = 2000; // Сумма на счете
         int replenishment = 10000; // Сумма пополнения
         int bonus;
-        int final_bill;
-        if (replenishment > 1000)
-        {
+        int finalBill;
+        if (replenishment > 1000) {
             bonus = replenishment / 100;
-            final_bill = bill + replenishment + bonus;
-                     System.out.println("Количество бонусов: " + bonus);
-            System.out.println("Сумма счета включая бонусы: " + final_bill);
-        }
-        else {
-            final_bill = bill + replenishment;
+            finalBill = bill + replenishment + bonus;
+            System.out.println("Количество бонусов: " + bonus);
+            System.out.println("Сумма счета включая бонусы: " + finalBill);
+        } else {
+            finalBill = bill + replenishment;
             System.out.println("Количество бонусов 0");
-            System.out.println("Сумма счета: " + final_bill);
+            System.out.println("Сумма счета: " + finalBill);
 
         }
 
